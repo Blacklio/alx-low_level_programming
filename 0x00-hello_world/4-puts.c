7e06df7875bf1bd6de2383476b45bfa578be2f94
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-09-12 08:47:40 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/** main - point of entering  return 0 - always 0 (success) */ 
+/** main - point of entering  return 0 - always 0 (success) */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
