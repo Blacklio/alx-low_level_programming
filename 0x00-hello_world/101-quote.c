5352ddced5167605479934c0da11da04b6d66183
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-09-12 09:22:39 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/** main - point of entering  return 0 - always 0 (success) */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (0);
+}
