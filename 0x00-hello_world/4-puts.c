7a8719d0f47e67c186367d478efeb5db658b2d0e
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-09-12 17:26:13 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/* main function - you can call other functions when the main returns the program stops /* return 0 - returns 0 (successs) */*/
+/** main function - you can call other functions when the main returns the program stops /* return 0 - returns 0 (successs) */*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
