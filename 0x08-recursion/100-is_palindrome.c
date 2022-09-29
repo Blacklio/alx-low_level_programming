58951ea234e2aac85aa4853757034081232950e6
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-09-28 20:50:48 -0700

    7. They say we only use a fraction of our brain

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * palind2 - obtains length of a
- * @a: string
- * @l: integer to count length
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* palind2 - obtains length of a
+* @a: string
+* @l: integer to count length
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int palind2(char *a, int l)
 {
 	if (*a == 0)
