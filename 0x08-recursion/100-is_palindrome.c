ab400ea72cec8fadbe94aa219a64a9fb4cc029ed
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-09-28 20:26:56 -0700

    7. They say we only use a fraction of our brain true potential

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,24 +1,41 @@
 #include "main.h"
+
+
+
 /**
+
  * palind2 - obtains length of a
+
  * @a: string
+
  * @l: integer to count length
+
+ *
+
  * Return: On success 1.
+
  * On error, -1 is returned, and errno is set appropriately.
+
  */
+
 int palind2(char *a, int l)
+
 {
+
 	if (*a == 0)
+
 		return (l - 1);
+
 	return (palind2(a + 1, l + 1));
+
 }
 /**
- * palind3 - compares string vs string reverse
- * @a: string
- * @l: length
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* palind3 - compares string vs string revers
+* @a: string
+* @l: length
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int palind3(char *a, int l)
 {
 	if (*a != *(a + l))
@@ -28,11 +45,14 @@ int palind3(char *a, int l)
 	return (palind3(a + 1, l - 2));
 }
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to evaluate
- * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
- */
+* is_palindrome - checks if a string is a palindrome
+* @s: string to evaluate
+* Return: On success 1
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int is_palindrome(char *s)
+{
+	int l;
 int is_palindrome(char *s)
 {
 	int l;
