199448338fa56845aa1a190aa24bfac723430753
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-04 05:08:44 -0700

    4. it is not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * free_grid - function to free memory 
- * @grid: pointer to grid of int type 
- * @height: int type 
+ * free_grid - function to free memory
+ * @grid: pointer to grid of int type
+ * @height: int type
  * Return: clean up memory
  */
 void free_grid(int **grid, int height)
 {
-	int y; 
+	int y;
 
 	if (grid == NULL || grid == 0)
 	{
