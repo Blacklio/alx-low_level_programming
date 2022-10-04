c3600a8c0e95da7a201cdbea945fdc20f367e337
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-04 13:26:31 -0700

    last

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,13 +1,97 @@
-#include <stdlib.h>
+#include "main.h"
+
 #include <stdio.h>
 
+#include <stdlib.h>
+
+
+
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
+
+ * argstostr - convert arguments on command line to strings 
+
+ * @ac: int type  
+
+ * @av: pointer to array 
+
+ * Return: arguments as strings 
+
  */
 
-int _strlen(char *s)
-{
-	int size = 0;
 
+
+char *argstostr(int ac, char **av)
+
+{
+
+	int size, count, count1, count2 = 0;
+
+	char *ptr; 
+
+
+
+	if (ac == 0 || av == NULL)
+
+	{
+
+		return (NULL);
+
+	} 
+
+
+
+	for (count = 0; count < ac; count++)
+
+	{
+
+		for (count1 = 0; av[count][count1] != '\0'; count1++)
+
+		{
+
+			size += 1;
+
+		}
+
+		size += 1;
+
+	}
+
+	size += 1;
+
+
+
+	ptr = malloc(sizeof(char) * size);
+
+	if (ptr == NULL)
+
+	{
+
+		free(ptr);
+
+		return (NULL);
+
+	}
+
+	for (count = 0; count < ac; count++)
+
+	{
+
+		for (count1 = 0; av[count][count1] != '\0'; count1++)
+
+		{
+
+			ptr[count2] = av[count][count1];count2++;
+
+		}
+
+		ptr[count2] = '\n';
+
+		count2++;
+
+	}
+
+	ptr[count2] = '\0';
+
+	return (ptr);
+
+}
