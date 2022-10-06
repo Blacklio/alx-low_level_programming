b8f268cda2a5f24c99e1e42c1125270d5db0e13c
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-06 00:23:16 -0700

    correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,12 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Prints a text according number
- * Return: Always (Success)
- */
+* main - Prints a text according number
+* return (0) - if 0 success
+*/
 int main(void)
 {
 	int n, lastd;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastd = n % 10;
@@ -21,7 +22,7 @@ int main(void)
 	}
 	else if (lastd < 6 && lastd != 0)
 	{
-        	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
 	return (0);
 }
