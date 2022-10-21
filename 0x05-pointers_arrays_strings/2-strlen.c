08572b9ec40978eb265ff86ca147587fce48d3a1
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-21 07:26:45 -0700

    last

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,11 +5,14 @@
  * @s: is a pointer int char type
  * Return: Always 0.
  */
+
 int _strlen(char *s)
 {
-int u;
-for (u = 0; *s != '\0'; u++)
-s++;
-return (u);
+	int u;
+
+	for (u = 0; *s != '\0'; u++)
+	{
+		s++;
+	}
+	return (u);
 }
-.
