e751a0884c9269b70ebe830114fc5c2303ed9cba
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-21 00:53:18 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/** main - point of entering  return 0 - always 0 (success) */
+/**
+ * main - point of entering
+ *
+ * Return 0 - always 0 (success)
+*/
 int main(void)
 {
 	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
