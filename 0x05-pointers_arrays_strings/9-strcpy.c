843d8504399e0d6b8889588992bfc2c8d0fb4e0c
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-21 08:03:41 -0700

    last

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,14 +5,17 @@
  * @dest: pointer char type
  * @src: pointer char type
  * Return: Always 0.
- */
+*/
+
 char *_strcpy(char *dest, char *src)
-int i;
-for (i = 0; *src != '\0'; i++)
 {
-dest[i] = *src;
-src++;
- }
-dest[i++] = *src;
-return (dest);
+	int i;
+
+	for (i = 0; *src != '\0'; i++)
+	{
+		dest[i] = *src;
+		src++;
+	}
+	dest[i++] = *src;
+	return (dest);
 }
