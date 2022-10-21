124e5448798303615ef5afcceeb79e847ed0310d
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-21 00:02:23 -0700

    last

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-  * main - Prints the numbers since 0 to 9
-  *
-  * Return: Always (Success)
-  */
-int main(void)
+ *main - prints the numbers from 0 to 9
+ *
+ *Return: Always (Success)
+ */
+int main(voiid)
 {
 	char n;
 
