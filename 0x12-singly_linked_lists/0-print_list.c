a13ab6254ce07e288207ace5fb055e5fa363a196
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-20 20:22:42 -0700

    last

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,14 +18,12 @@ size_t print_list(const list_t *h)
 		{
 			printf("[%d] %s\n", 0, "(nil)");
 			tmp = tmp->next;
-
 			i++;
 		}
 		else
 		{
 			printf("[%d] %s\n", tmp->len, tmp->str);
 			tmp = tmp->next;
-
 			i++;
 		}
 	}
