cb90db8aca6330b1628d6ad2fedde4ec690c1cf0
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-21 00:46:35 -0700

    last

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/** main - point of entering  return 0 - always 0 (success) */
+/**
+ * main - point of entering text
+ *
+ * Return: Always 0 (success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
