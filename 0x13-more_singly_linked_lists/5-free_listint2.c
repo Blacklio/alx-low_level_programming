ca6e751ac9f841eda3d0c29e6f0b7dc4b5b1f456
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-25 19:00:22 +0300

    last

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,15 +6,16 @@
  * Return: 0 elements all free
  */
 
-void free_listint2(listint_t **head)
+void free_listint2(listint_t** head)
 {
+	listint_t* temp;
+
 	if (head == NULL)
 		return;
-	while (*head)
+	while (*head != NULL)
 	{
+		temp = (*head)->next;
 		free(*head);
-		*head = (*head)->next;
-
+		*head = temp;
 	}
-	head = NULL;
 }
