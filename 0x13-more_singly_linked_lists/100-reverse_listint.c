8237ecbf481bdd9c3f5f284acf85365cada5359a
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-25 20:21:01 +0300

    last

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,65 +7,13 @@
  * Return: always successful
  */
 
-struct Node {
-    int data;
-    struct Node* next;
-};
-
-// newNode function inserts the new node at
-// the right side of linked list
-struct Node* newNode(int key)
+struct listint_s 
 {
-    struct Node* temp = new Node;
-    temp->data = key;
-    temp->next = NULL;
-    return temp;
+	int n;
+	struct listint_s* next;
 }
+typedef struct listint_s listint_t;
 
-// reverse() will receive individual data item
-// from reverseIndividualData() and will return
-// the reversed number to calling function
-int reverse(int number)
-{
-    int new_num = 0, rem;
+/* Function to reverse the linkedlist */
 
-    while (number != 0) {
-        rem = number % 10;
-        new_num = new_num * 10 + rem;
-        number = number / 10;
-    }
-
-    return new_num;
-}
-
-void reverseIndividualData(struct Node* node)
-{
-
-    if (node == NULL)
-        return;
-
-    while (node != NULL) {
-
-        /*  function call to reverse(),
-            reverseIndividualData(struct Node *node)
-            will send the one data item at a time to
-            reverse(node->data) function which will
-            return updated value to node->data*/
-
-        node->data = reverse(node->data);
-
-        /*  updating node pointer so as to get
-            next value */
-
-        node = node->next;
-    }
-}
-
-// Function to print nodes in linked list
-void printList(struct Node* node)
-{
-    while (node != NULL) {
-        printf("%d", node->data);
-        node = node->next;
-    }
-}
+void reverse(listint_t** head)
