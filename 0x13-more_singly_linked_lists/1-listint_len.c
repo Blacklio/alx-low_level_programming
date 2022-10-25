75843e29a13e9ea90da05df886301a0ae3afdfbc
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-25 16:47:51 +0300

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,12 +5,12 @@
  * Return: elements of the str i
  */
 
-size_t listint_len(const listint_t *h)
+size_t listint_len(const listint_t* h)
 {
 	unsigned int i = 0;
-	const listint_t	 *new_node = h;
+	const listint_t* new_node = h;
 
 	for (; new_node; new_node = new_node->next)
 		i++;
-		return (i);
+	return (i);
 }
