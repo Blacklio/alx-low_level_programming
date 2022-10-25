6b8d298384eb702061d1351256186e32bf74ac49
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-25 16:50:28 +0300

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,9 @@
 #include "lists.h"
-
 /**
  * listint_len - Return the number of elements.
  * @h: data type pointer of struct.
  * Return: elements of the str i.
  */
-
 size_t listint_len(const listint_t* h)
 {
 	unsigned int i = 0;
