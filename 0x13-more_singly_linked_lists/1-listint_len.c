4fafa69a25848b8a051036c21b1dedfec039906c
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-10-25 18:32:04 +0300

    last

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,6 +8,7 @@
 size_t listint_len(const listint_t* h)
 {
 	const listint_t* tp;
+
 	unsigned int cnr = 0;
 
 	tp = h;
@@ -15,9 +16,7 @@ size_t listint_len(const listint_t* h)
 	while (tp)
 	{
 		cnr++;
-
 		tp = tp->next;
-
 	}
 	return (cnr);
 }
