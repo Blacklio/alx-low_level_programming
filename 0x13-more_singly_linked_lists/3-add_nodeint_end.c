d6f517fb016b70e51d2891738fe4b89afa6ead1a
Elias Eshetu Mammo <eliasmamo21+alx@gmail.com>
2022-11-18 20:42:21 -0800

    last

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,95 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
-<<<<<<< HEAD
-
-
+#include <string.h>
 
 /**
-
- * add_nodeint_end - Adds a node at the begining of a listint_t.
-
- * @n: data type pointer of struct
-
- * @head: data type pointer of pointer the head/next node
-
- * Return: elements of the str new_node
-
- */
-
-listint_t *add_nodeint_end(listint_t **head, const int n)
-
-{
-
-	listint_t *new_node, *tmp_node;
-
-
-
-	new_node = malloc(sizeof(listint_t));
-
-	if (new_node == NULL)
-
-		return (NULL);
-
-
-
-	new_node->n = n;
-
-	new_node->next = NULL;
-
-	if (*head == NULL)
-
-	{
-
-		*head = new_node;
-
-	}
-
-	else
-
-	{
-
-		tmp_node = *head;
-
-		while (tmp_node->next != NULL)
-
-=======
-/**
- * add_nodeint_end - Adds a node at the begining of a listint_t.
- * @n: data type pointer of struct.
- * @head: data type pointer of pointer the head/next node.
- * Return: elements of the str new_node.
+ * add_nodeint_end - function that adds a new node at the end of a listint_t
+ * @head: input header pointer
+ * @n: Input data int
+ * Return: the address of the new element, or NULL if it failed
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node, *tmp_node;
+	listint_t *newNode;
+	listint_t *tmp;
 
-	new_node = malloc(sizeof(listint_t));
-
-	if (new_node == NULL)
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
 		return (NULL);
-
-	new_node->n = n;
-	new_node->next = NULL;
+	newNode->n = n;
+	newNode->next = NULL;
 	if (*head == NULL)
-	{
-		*head = new_node;
-	}
+		*head = newNode;
 	else
 	{
-		tmp_node = *head;
-
-		while (tmp_node->next != NULL)
->>>>>>> 508a638612b6975cac7099369f6c8a3ff42b7f8f
-			tmp_node = tmp_node->next;
-
-		tmp_node->next = new_node;
-
+		tmp = *head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = newNode;
 	}
-<<<<<<< HEAD
-
-	return (new_node);
-
-=======
-	return (new_node);
->>>>>>> 508a638612b6975cac7099369f6c8a3ff42b7f8f
+	return (newNode);
 }
--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,66 +1,20 @@
 #include <stdlib.h>
-<<<<<<< HEAD
-
 #include "lists.h"
 
-
-
 /**
-
-  * free_listint - Frees a linked list
-
-  * @head: The head of the linked list
-
-  *
-
-  * Return: Nothing
-
-  */
-
-void free_listint(listint_t *head)
-
-{
-
-	listint_t *temp;
-
-
-
-	while (head)
-
-	{
-
-		temp = head;
-
-		head = head->next;
-
-		free(temp);
-
-	}
-
-
-
-	free(head);
-
-=======
-#include "lists.h"
-/**
-  * free_listint - Frees a linked list
-  * head: The head of the linked list
-  *
-  * Return: Nothing
-  */
+ * free_listint - function that frees a listint_t list
+ * @head: input header pointer
+ * Return: nothing
+ */
 
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *tmp;
 
-	while (head)
+	while (head != NULL)
 	{
-		temp = head;
+		tmp = head;
 		head = head->next;
-
-		free(temp);
+		free(tmp);
 	}
-	free(head);
->>>>>>> 508a638612b6975cac7099369f6c8a3ff42b7f8f
 }
