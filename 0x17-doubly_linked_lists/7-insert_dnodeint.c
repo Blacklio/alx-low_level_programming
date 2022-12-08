5b2e9931309808976cd2af06062b67d27ba38074
EliasEsheetuMammo <eliasmamo21+alx@gmail.com>
2022-12-08 11:58:05 -0800

    doubly looped lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,32 +1,35 @@
-#include "lists.h"
-
-/**
- * insert_dnodeint_at_index - inserts a new node at given index in the list
- * @head: pointer to head of the list
- * @idx: index to add at, starting from 0
- * @n: value of new node
- * Return: new node or null
- **/
-
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
-{
-	unsigned int count;
-	dlistint_t *tmp, *new, *tmp_prev;
-
-	if (head == NULL && idx > 0)
-	return (NULL);
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n, new->prev = new->next = NULL;
-
-	if (idx == 0)
-	{
+include "lists.h"                                                                                                                                                                                                                                                
+                                                                                                                                                                                                                                                                  
+/**                                                                                                                                                                                                                                                               
+ * insert_dnodeint_at_index - inserts a new node at given index in the list                                                                                                                                                                                       
+ * @head: pointer to head of the list                                                                                                                                                                                                                             
+ * @idx: index to add at, starting from 0                                                                                                                                                                                                                         
+ * @n: value of new node                                                                                                                                                                                                                                          
+ * Return: new node or null                                                                                                                                                                                                                                       
+ **/                                                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                                                  
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)                                                                                                                                                                                  
+{                                                                                                                                                                                                                                                                 
+        unsigned int count;                                                                                                                                                                                                                                       
+        dlistint_t *tmp, *new, *tmp_prev;                                                                                                                                                                                                                         
+                                                                                                                                                                                                                                                                  
+        if (head == NULL && idx > 0)                                                                                                                                                                                                                              
+        return (NULL);                                                                                                                                                                                                                                            
+                                                                                                                                                                                                                                                                  
+        new = malloc(sizeof(dlistint_t));                                                                                                                                                                                                                         
+                                                                                                                                                                                                                                                                  
+        if (new == NULL)                                                                                                                                                                                                                                          
+                return (NULL);                                                                                                                                                                                                                                    
+        new->n = n, new->prev = new->next = NULL;                                                                                                                                                                                                                 
+                                                                                                                                                                                                                                                                  
+        if (idx == 0)                                                                                                                                                                                                                                             
+        { 
 		if (*head)
-	{
-		new->next = *head;
-		(*head)->prev = new, *head = new;
-	}
+		{
+			new->next = *head;
+			(*head)->prev = new, *head = new;
+
+		}
 		else
 			*head = new;
 		return (new);
