1add47974fb0c437e738d58c3a0d176bdc968a0d
EliasEsheetuMammo <eliasmamo21+alx@gmail.com>
2023-01-06 19:11:41 -0800

    advanced

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -8,7 +8,36 @@
  * Return: If an error occurs - NULL.
  *         Otherwise - a pointer to the new sorted hash table.
  */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	shash_table_t *ht;
+	unsigned long int i;
 
+	ht = malloc(sizeof(shash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(shash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+	ht->shead = NULL;
+	ht->stail = NULL;
+
+	return (ht);
+}
+
+/**
+ * shash_table_set - Adds an element to a sorted hash table.
+ * @ht: A pointer to the sorted hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
+ */
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
 	shash_node_t *new, *tmp;
@@ -92,7 +121,6 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
  * Return: If the key cannot be matched - NULL.
  *         Otherwise - the value associated with key in ht.
  */
-
 char *shash_table_get(const shash_table_t *ht, const char *key)
 {
 	shash_node_t *node;
@@ -116,7 +144,6 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
  * shash_table_print - Prints a sorted hash table in order.
  * @ht: A pointer to the sorted hash table.
  */
-
 void shash_table_print(const shash_table_t *ht)
 {
 	shash_node_t *node;
@@ -140,7 +167,6 @@ void shash_table_print(const shash_table_t *ht)
  * shash_table_print_rev - Prints a sorted hash table in reverse order.
  * @ht: A pointer to the sorted hash table to print.
  */
-
 void shash_table_print_rev(const shash_table_t *ht)
 {
 	shash_node_t *node;
@@ -164,7 +190,6 @@ void shash_table_print_rev(const shash_table_t *ht)
  * shash_table_delete - Deletes a sorted hash table.
  * @ht: A pointer to the sorted hash table.
  */
-
 void shash_table_delete(shash_table_t *ht)
 {
 	shash_table_t *head = ht;
