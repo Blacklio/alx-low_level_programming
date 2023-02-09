633a3a26fae498686ee541cd238fc5caea29c7d9
EliasEsheetuMammo <eliasmamo21+alx@gmail.com>
2023-02-09 01:02:17 -0800

    all

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,35 +1,18 @@
-#include <stdlib.h>
-
-#include <stdio.h>
-
 #include "main.h"
 
-
-
 /**
-
- * set_bit - Sets the value of a bit to 1 at a given index
-
- * @n: The number to modify
-
- * @index: The index in the number to modify
-
- *
-
- * Return: 1 if it worked, or -1 if an error occurred
-
- */
+*set_bit - sets a bit at position to 1
+*@n: number to be used
+*@index: index to be set
+*
+*Return: 1 on success, -1 on failure
+*/
 
 int set_bit(unsigned long int *n, unsigned int index)
-  
 {
-  
-  if (index > sizeof(unsigned long int) * 8)
-    
-    return (-1);
-  
+	if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
+		return (-1);
 
-  
-  return ((*n |= 1 << index) ? 1 : -1);
-  
+	*n |= (1 << index);
+	return (1);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,49 +1,23 @@
-#include <stdlib.h>
-
 #include "main.h"
 
-
-
 /**
-
- * flip_bits - ...
-
- * @n: ...
-
- * @m: ...
-
- *
-
- * Return: ...
-
- */
+*flip_bits - gets the number of bits to flip to get from n to m
+*@n: initial number
+*@m: final number
+*
+*Return: the number of flipped bits
+*/
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
-  
 {
-  
-  unsigned long int mask = 0, count = 0;
-  
+	unsigned long int flipped = n ^ m;
+	int count = 0;
 
-  
-  mask = n ^ m;
-  
-  while (mask)
-    
-    {
-      
-      if (mask & 1)
-	
-	count++;
-      
-
-      
-      mask >>= 1;
-      
-    }
-  
-
-  
-  return (count);
-  
+	while (flipped)
+	{
+		if (flipped & 1)
+			count++;
+		flipped >>= 1;
+	}
+	return (count);
 }
